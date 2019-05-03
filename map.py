4dbf540076bb71d3bbba0a4ff624866d12dd085e
799115 <roboboy@kalayjian.org>
2019-05-03 11:40:59 -0700

    friday

--- map.py
+++ map.py
@@ -1,16 +1,39 @@
 from util import *
-
+import random
 class Map(object):
-
 
 
 	def __init__(self, sX,sY):
 
-		self.generateMap(sX,sY)
 
 		self.tileSize = 32
-		self.sizeX = len(self.tiles[0])
-		self.sizeY = len(self.tiles)
+
+
+	def loadRooms(self,filename):
+		# seperate txt into array of room configurations
+		file = open("rooms.txt","r")
+
+
+		self.roomConfigurations = []
+
+
+		lines = file.readlines() 
+
+		loadedRoom = []
+		for line in lines:
+			if "&" not in line:
+				loadedRoom.append([int(i) for i in line[:len(line)-1]])
+			else:
+				self.roomConfigurations.append(loadedRoom)
+				loadedRoom = []
+		print(self.roomConfigurations)
+
+
+
+
+
+
+
 	def generateMap(self,sx,sy): # generates map with closed walls and random obstacles, temporary before BSP or loading map
 		for y in range(sy):
 			row = []
@@ -34,19 +57,26 @@ class Map(object):
 					pygame.draw.rect(cvs,(0,0,255),(tilePosX,tilePosY,self.tileSize,self.tileSize))
 	class Room:
 
-		def __init__ (self, tiles, parent=None):
-			self.tiles = tiles
+		def __init__ (self, parent=None):
+			self.tiles = []
 			self.parent = parent
 
 			
-			self.bbox = Rect(min(t.x for t in tiles), min(t.y for t in tiles), max(t.x for t in tiles), max(t.y for t in tiles))
+			#self.bbox = Rect(min(t.x for t in tiles), min(t.y for t in tiles), max(t.x for t in tiles), max(t.y for t in tiles))
+		def generateRoom(self,x,y,spawnOnDoor=False):
 
 			
 
 
 	class Tile:
+
 		def __init__ (self,x,y,t):
 			self.x = x
 			self.y = y
 			self.type = t
 
+
+m = Map(50, 50)
+
+
+m.loadRooms("rooms.txt")
