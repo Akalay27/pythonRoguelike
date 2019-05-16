2f123d09bf0a770cba59e2579b8ece69bd5a900f
799115 <roboboy@kalayjian.org>
2019-05-16 11:43:37 -0700

    animationcontrollerstuff

--- animationController.py
+++ animationController.py
@@ -1,7 +1,35 @@
+import time
+
 class AnimationController(object):
 	"""controls the animations"""
 	def __init__(self):
 		self.states = []
-	def addState(self,sprites,speed,loop,
+	
+	def addAnimationState(self,tilemap,resolution,speed,loop=False)
+
+		
+
+
+	class AnimationState(object):
+		'''stores a single set of sprites and properties including speed and if looping'''
+		def __init__(self,sprites,speed,loop):
+			self.sprites = sprites
+			self.speed = speed
+			self.loop = loop
+			self.active = False
+
+
+		def begin(self):
+			self.active = True
+			self.startTime = time.time()
+			self.frame = 0
+		def getFrame(self):
+
+			frame = ((time.time()-self.startTime)/self.speed)%len(self.sprites)
+
+			return self.sprites(frame)
+
+
+
 
 		
